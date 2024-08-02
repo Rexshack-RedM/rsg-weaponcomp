85cde46df44dbda7bed852948abe620f53616fa9
RexshackGaming <rexshackuk@gmail.com>
2024-08-02 11:10:23 +0100

    version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,9 @@
 fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
 game 'rdr3'
 
 description 'rsg-weaponcomp'
-version '1.0.5'
+version '1.0.6'
 
 shared_script {
     'config.lua',
